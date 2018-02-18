ec3d47d4e0bf80d4bbf8e5a0042d0100a826b78d
nattamon sridam <fernvnat@Fernvnats-MacBook-Pro.local>
2018-02-18 23:13:38 +0700

    Initial Commit

---
+++ LabhttpServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LabhttpServer
+//
+//  Created by Fernvnat on 18/2/2561 BE.
+//  Copyright © 2561 Fernvnat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
